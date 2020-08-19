c0a30b94be9eb98dc35675c94497d0f9331a2b05
Paulo Rodrigues <pauloadesr@hotmail.com>
2020-08-19 10:35:17 -0300

    code pattern

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>RN</Text>
+    </View>
+  );
+};
+
+export default App;
