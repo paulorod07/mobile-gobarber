125c3614c15b48bf4e4b0b406ee0f87820dfd54b
Paulo Rodrigues <pauloadesr@hotmail.com>
2020-08-19 11:14:13 -0300

    routes and react-navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>RN</Text>
-    </View>
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
   );
 };
 
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <View>
+      <Text />
+    </View>
+  );
+};
+
+export default SignUp;
